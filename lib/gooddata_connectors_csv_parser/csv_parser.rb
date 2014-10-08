aaffdd6671a0f9a942283a8fb11e853b4b089ec3
adriantoman <adrian.toman@gooddata.com>
2014-10-08 16:35:52 +0200

    CSV Parser - v0.2 not fully functional yet

--- lib/gooddata_connectors_csv_parser/csv_parser.rb
+++ lib/gooddata_connectors_csv_parser/csv_parser.rb
@@ -20,20 +20,28 @@ module GoodData
           }
         end
 
+
         def parse_entity(entity)
+          parse_entity_data(entity)
+          parse_entity_deleted_records(entity)
+          parse_entity_synchronization_records(entity)
+        end
+
+
+        def parse_entity_data(entity)
           begin
             source_csv = CSV.open(entity.runtime["source_filename"],:headers => true)
             source_csv_enumerator = source_csv.each
             output_filename = "#{@data_directory}#{entity.id}.csv"
             CSV.open(output_filename,"wb",:headers => true) do |csv|
-              csv << header(entity)
+              csv << header(entity,entity.get_enabled_fields)
               source_csv_enumerator.each do |row|
-                parse_row(entity,row).each do |output_row|
+                parse_row(entity,row,entity.get_enabled_fields).each do |output_row|
                   csv << output_row
                 end
               end
             end
-            entity.runtime["parsed_filename"] = "#{@data_directory}#{entity.id}.csv"
+            entity.runtime["parsed_filename"] = output_filename
           rescue => e
               pp e.message
               fail "ups"
@@ -43,16 +51,73 @@ module GoodData
         end
 
 
+
+
+
+        def parse_entity_deleted_records(entity)
+          if (entity.runtime.include?("source_deleted_filename"))
+            begin
+              fields = [entity.custom["id"],entity.custom["timestamp"],"IsDeleted"]
+              source_csv = CSV.open(entity.runtime["source_deleted_filename"],:headers => true)
+              source_csv_enumerator = source_csv.each
+              output_filename = "#{@data_directory}#{entity.id}_deleted.csv"
+              CSV.open(output_filename,"wb",:headers => true) do |csv|
+                csv << header(entity,fields)
+                source_csv_enumerator.each do |row|
+                  parse_row(entity,row,fields).each do |output_row|
+                    csv << output_row
+                  end
+                end
+              end
+              entity.runtime["deleted_parsed_filename"] = output_filename
+            rescue => e
+              pp e.message
+              fail "ups"
+            ensure
+              source_csv.close
+            end
+          end
+        end
+
+
+        def parse_entity_synchronization_records(entity)
+          if (entity.runtime.include?("synchronization_source_filename"))
+            begin
+              new_fields = entity.fields.values.find_all{|f| !f.disabled? and f.custom["synchronized"] == false }
+              new_fields << entity.get_field(entity.custom["id"])
+              new_fields << entity.get_field(entity.custom["timestamp"])
+              source_csv = CSV.open(entity.runtime["synchronization_source_filename"],:headers => true)
+              new_fields_ids = new_fields.map{|v| v.id}
+              source_csv_enumerator = source_csv.each
+              output_filename = "#{@data_directory}#{entity.id}_synchronization.csv"
+              CSV.open(output_filename,"wb",:headers => true) do |csv|
+                csv << header(entity,new_fields_ids)
+                source_csv_enumerator.each do |row|
+                  parse_row(entity,row,new_fields_ids).each do |output_row|
+                    csv << output_row
+                  end
+                end
+              end
+              entity.runtime["synchronization_parsed_filename"] = output_filename
+            rescue => e
+              pp e.message
+              fail "ups"
+            ensure
+              source_csv.close
+            end
+          end
+        end
+
+
+
         private
 
 
-        def parse_row(entity,source_row)
+        def parse_row(entity,source_row,fields)
           rows = []
           custom_entity_settings = entity.custom
           if (!entity.dependent_on.nil?)
-
             type = ""
-
             if (custom_entity_settings.include?("type"))
               type = custom_entity_settings["type"]
             else
@@ -79,12 +144,15 @@ module GoodData
               source_file_metadata_fields += custom_entity_settings["ignored"] if custom_entity_settings.include?("ignored")
               fields_to_normalize = entity_field - source_file_metadata_fields
               fields_to_normalize.each do |field_name|
-
-
                 row = CSV::Row.new(Base::Global::HISTORY_FILE_STRUCTURE,[],false)
                 Base::Global::HISTORY_FILE_STRUCTURE.each do |history_field_name|
                   if (history_field_name == "value")
-                    row[history_field_name] = source_row[field_name]
+                    if (source_row[field_name] == "")
+                      field_type = entity.get_field(field_name).type
+                      row[history_field_name] = field_type.default
+                    else
+                      row[history_field_name] = source_row[field_name]
+                    end
                   elsif (history_field_name == "key")
                     row[history_field_name] = field_name
                   else
@@ -103,8 +171,8 @@ module GoodData
               raise Base::ParseException,"Unknown history type for entity #{entity.id}. Type value is #{type} supported (normalized,denormalized)"
             end
           else
-            row = CSV::Row.new(entity.get_enabled_fields,[],false)
-            entity.get_enabled_fields.each do |field_name|
+            row = CSV::Row.new(fields,[],false)
+            fields.each do |field_name|
               row[field_name] = source_row[field_name]
             end
             rows << row
@@ -112,15 +180,14 @@ module GoodData
           rows
         end
 
-        def header(entity)
+        def header(entity,fields)
           custom_entity_settings = entity.custom
           if (custom_entity_settings.include?("dependent_on"))
             CSV::Row.new(Base::Global::HISTORY_FILE_STRUCTURE,Base::Global::HISTORY_FILE_STRUCTURE,true)
           else
-            CSV::Row.new(entity.get_enabled_fields,entity.get_enabled_fields,true)
+            CSV::Row.new(fields,fields,true)
           end
         end
-
 
 
 
